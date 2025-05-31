f9c3c5260f84ca504092ce9c6b35f04d764c67be
Mikhail <ekkemeev2014@yandex.ru>
2025-05-31 11:58:15 +0300

    Выполнение итогового задания: часть один

---
+++ src/main/java/ru/iFellow/config/AppConfig.java
@@ -0,0 +1,22 @@
+package ru.iFellow.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config.properties")
+public interface AppConfig extends Config {
+
+    @Key("page.load.timeout")
+    long pageLoadTimeout();
+
+    @Key("base.url")
+    String baseUrl();
+
+    @Key("login.end.point")
+    String endPointLogin();
+
+    @Key("user.password")
+    String password();
+
+    @Key("user.login")
+    String login();
+}
